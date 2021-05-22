8cbdeca2c6aeae54a5795013ec504d5c77d5a0d7
deepvn98 <deepvn988@gmail.com>
2021-05-22 17:31:05 +0700

    baitap

---
+++ src/main/java/Discount.java
@@ -0,0 +1,30 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Discount", value = "/Discount")
+public class Discount extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        String namesp = request.getParameter("tensp");
+        double price;
+                price = Double.parseDouble(request.getParameter("prince"));
+
+        double saleoff;
+                saleoff = Double.parseDouble(request.getParameter("saleoff"));
+//        if (namesp != null) {
+            double discountAmount = price * saleoff * 0.01;
+            double discountPrice = price - discountAmount;
+            writer.println("DiscountAmount: " + discountAmount);
+            writer.println("DiscountPrice: " + discountPrice);
+//        }
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 22/5/2021
+  Time: 2:48 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <h1>Information</h1>
+  <br>
+  <form action="Discount" method="post">
+    <input type="text" name="tensp" placeholder="Tên Sp"> <br> <br>
+    <input type="number" name="price" placeholder="Giá sp"> <br> <br>
+    <input type="number" name="saleoff" placeholder="sale off"> <br><br>
+    <input type="submit" name="submit" id="submit">
+
+  </form>
+  </body>
+</html>
